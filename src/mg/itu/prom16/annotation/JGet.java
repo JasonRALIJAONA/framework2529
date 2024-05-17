d2057af922a1ef6a419800f930f005ef06a38b2f
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-05-17 00:09:11 -0900

    annotation,HashMap

---
+++ src/mg/itu/prom16/annotation/JGet.java
@@ -0,0 +1,10 @@
+package mg.itu.prom16.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JGet {
+    String path() ;
+}
+
